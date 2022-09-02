2032b222da1e929e7ba88ce9fb52d6a3d275187b
cjones1047 <cjmj1047@gmail.com>
2022-09-02 13:28:53 -0500

    List of traits for each project added

---
+++ src/api/project-list.js
@@ -0,0 +1,36 @@
+const projects = [
+    {
+        title: 'Caddystack',
+        description: [
+            `Created a MERN (MongoDB, Express, React.js, Node.js) stack that allowed a user to post tee times for any golf course for sale and buy other users’ posted tee times`, 
+            `Front-end React app utilized Bootstrap and Material UI as styling libraries`, 
+            `Used Golf Course Finder API to get JSON data on lists of golf courses within a certain radius of a certain zip code`,
+            `Golf Course Finder API queried to get data on individual golf courses`,
+            `Queried Forward & Reverse Geocoding API to convert zip codes to specific coordinates of longitude and latitude`,
+            `Made RESTful API fetch calls to an Express, MongoDB, Node.js backend handling all create, read, update, delete (CRUD) operations, and tested all backend routes during development using Postman.`,
+            `Used Mongoose to define and implement three different schemas for separate MongoDB collections.`,
+            `Implemented ES6 tagged template literals along with JSX and CSS3 to provide style and flexbox layout of the application.`, 
+            `Back-end MEN stack created with MongoDB, Express, Node.js and queried with Axios requests`,
+            `Front-end and back-end both deployed via Heroku`,
+            `Leveraged Git and GitHub for version control`
+        ],
+        screenshot: '',
+        link: 'https://caddystack.herokuapp.com/'
+    },
+    {
+        title: 'Stockpile',
+        description: [
+            `Used QuickFS API to fetch recent ten years of any searched company’s financial data`,
+            `Used Puppeteer to web scrape most-recent market share price`,
+            `Wireframed for the planning process of the UI design for the application.`,
+            `Created the front-end of the application utilizing Javascript array methods and functions to handle all data received from API and back-end along with Liquid for rendering on the front-end.`,
+            `Served the back-end of the application using Express, MongoDB, and Node.js`,
+            `Made all fetch API calls from the back-end to send data to the front-end.`,
+            `Utilized HTML5 and Bootstrap, CSS3, and jQuery to create overall visual aspects and animations.`
+        ],
+        screenshot: '',
+        link: 'https://stockpile-1047.herokuapp.com/'
+    }
+]
+
+export default projects
--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -10,40 +10,7 @@ import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 
-const projects = [
-    {
-        title: 'Caddystack',
-        description: [
-            `Created a MERN (MongoDB, Express, React.js, Node.js) stack that allowed a user to post tee times for any golf course for sale and buy other users’ posted tee times`, 
-            `Front-end React app utilized Bootstrap and Material UI as styling libraries`, 
-            `Used Golf Course Finder API to get JSON data on lists of golf courses within a certain radius of a certain zip code`,
-            `Golf Course Finder API queried to get data on individual golf courses`,
-            `Queried Forward & Reverse Geocoding API to convert zip codes to specific coordinates of longitude and latitude`,
-            `Made RESTful API fetch calls to an Express, MongoDB, Node.js backend handling all create, read, update, delete (CRUD) operations, and tested all backend routes during development using Postman.`,
-            `Used Mongoose to define and implement three different schemas for separate MongoDB collections.`,
-            `Implemented ES6 tagged template literals along with JSX and CSS3 to provide style and flexbox layout of the application.`, 
-            `Back-end MEN stack created with MongoDB, Express, Node.js and queried with Axios requests`,
-            `Front-end and back-end both deployed via Heroku`,
-            `Leveraged Git and GitHub for version control`
-        ],
-        screenshot: '',
-        link: 'https://caddystack.herokuapp.com/'
-    },
-    {
-        title: 'Stockpile',
-        description: [
-            `Used QuickFS API to fetch recent ten years of any searched company’s financial data`,
-            `Used Puppeteer to web scrape most-recent market share price`,
-            `Wireframed for the planning process of the UI design for the application.`,
-            `Created the front-end of the application utilizing Javascript array methods and functions to handle all data received from API and back-end along with Liquid for rendering on the front-end.`,
-            `Served the back-end of the application using Express, MongoDB, and Node.js`,
-            `Made all fetch API calls from the back-end to send data to the front-end.`,
-            `Utilized HTML5 and Bootstrap, CSS3, and jQuery to create overall visual aspects and animations.`
-        ],
-        screenshot: '',
-        link: 'https://stockpile-1047.herokuapp.com/'
-    }
-]
+import projects from '../../api/project-list';
 
 function TabPanel(props) {
     const { children, value, index, ...other } = props;
@@ -98,7 +65,22 @@ const Project = (props) => {
 
     const tabs = projects.map((project, i) => {
         return (
-            <Tab label={project.title} key={i}  style={{fontWeight: 600, width: '400px'}} {...a11yProps(i)} />
+            <Tab label={project.title} key={i}  style={{fontWeight: 600, width: '250px'}} {...a11yProps(i)} />
+        )
+    })
+
+    const tabPanels = projects.map((project, i) => {
+        return (
+            <TabPanel value={value} key={i} index={i} dir={theme.direction}>
+                {projects[i].description.map((listItem, i) => {
+                    return (
+                        <li key={i}>
+                            {listItem}
+                        </li>
+                    )
+                })}
+                <a href={project.link}>{project.link}</a>
+            </TabPanel>
         )
     })
 
@@ -109,9 +91,8 @@ const Project = (props) => {
                 onChange={handleChange}
                 indicatorColor="primary"
                 textColor="primary"
-                variant={viewportWidth < (400*projects.length) ? "scrollable" : "standard"}
-                scrollButtons={viewportWidth < (400*projects.length) ? true : false}
-                // variant="scrollable"
+                variant={viewportWidth < (250*projects.length) ? "scrollable" : "standard"}
+                scrollButtons={viewportWidth < (250*projects.length) ? true : false}
                 aria-label="project tabs"
                 allowScrollButtonsMobile={true}
                 sx={{
@@ -119,10 +100,9 @@ const Project = (props) => {
                         '&.Mui-disabled': { opacity: 0.3 },
                     },
                 }}
-                // style={{fontWeight: 600}}
-                // center tabs if viewport width is greater than one tab's width (200px) multiplied by however many tabs there are
-                // set to scrollable if all tabs combined to be wider than viewport width
-                centered={viewportWidth > (400*projects.length) ? true : false}
+                // above attributes ternary operators ^^^ set variant to scrollable if all tabs combined to be wider than viewport width
+                // below, we will center tabs if viewport width is greater than one tab's width (200px) multiplied by however many tabs there are
+                centered={viewportWidth > (250*projects.length) ? true : false}
                 
             >
                 {/* <Tab label="Item One"  style={{fontWeight: 600, }} {...a11yProps(0)} />
@@ -136,7 +116,8 @@ const Project = (props) => {
                 index={value}
                 onChangeIndex={handleChangeIndex}
             >
-                <TabPanel value={value} index={0} dir={theme.direction}>
+                {tabPanels}
+                {/* <TabPanel value={value} index={0} dir={theme.direction}>
                     Item One
                 </TabPanel>
                 <TabPanel value={value} index={1} dir={theme.direction}>
@@ -147,7 +128,7 @@ const Project = (props) => {
                 </TabPanel>
                 <TabPanel value={value} index={3} dir={theme.direction}>
                     Item Four
-                </TabPanel>
+                </TabPanel> */}
             </SwipeableViews>
         </Box>
     );
